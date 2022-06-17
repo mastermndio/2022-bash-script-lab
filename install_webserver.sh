fccee240c59b8e05c06d2015ac4b4b10a38447b7
Aaron B <abrooks@mastermnd.io>
2022-06-17 02:02:22 +0000

    Added auto distro identification

--- install_webserver.sh
+++ install_webserver.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-DISTRO=$1
-
-if [[ $DISTRO == "ubuntu" ]]; then
+if [[ $(grep -i ubuntu /etc/*-release | wc -l) > 0 ]]; then
 	echo "Youre running ubuntu, using apt to install nginx"
 	PKG_MANAGER="apt"
 	PKG_NAME="nginx"
-elif [[ $DISTRO == "redhat" ]]; then
+else
 	echo "Youre running redhat, using yum to install apache"
 	PKG_MANAGER="yum"
 	PKG_NAME="httpd"
